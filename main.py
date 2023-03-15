291d5a33965bcb6e30633223e187ff3ad2a105c6
aleksandrkosanin <kosyanin@yandex.ru>
2023-03-16 02:12:41 +0300

    added comments

--- main.py
+++ main.py
@@ -11,21 +11,24 @@ openai.api_key = openai_api_key
 bot = Bot(token)
 dp = Dispatcher(bot)
 
+
 async def start_bot(_):
     print('Бот запущен')
 
 
+# Отправляем сообщение при запуске бота
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await bot.send_message(message.from_user.id,
                            "Привет! Скажи, что мне что-нибудь и я это нарисую. Русский язык понимаю очеь плохо, так что"
                            " лучше пиши на английском. Генерация картинки занимает чуть больше 10 секунд.")
 
-
+# Любое сообщение от пользователя воспринимаем как описание картинки
 @dp.message_handler()
 async def send(message: types.Message):
-    prompt = message.text
     try:
+        # Генерируем изображение
+        prompt = message.text
         response = openai.Image.create(
             prompt=prompt,
             model="image-alpha-001",
@@ -40,10 +43,10 @@ async def send(message: types.Message):
         await bot.send_message(message.from_user.id, "нуу, такое не осилю, давай что-то другое")
 
 
-# Запуск процесса поллинга новых апдейтов
+# Запускаем процесс поллинга новых апдейтов
 async def main():
     await dp.start_polling(bot)
 
-
+# Запускаем бота
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=start_bot)
